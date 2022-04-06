0eab147e0b861924b8c53ef0589bc5080f6e9d48
hyeri <rhkk7157@gmail.com>
2022-04-06 13:12:00 +0900

    metadata setting

--- contracts/MintGemToken.sol
+++ contracts/MintGemToken.sol
@@ -3,12 +3,34 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MintGemToken is ERC721Enumerable {
-  constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+  string uri;
+  constructor(string memory _name, string memory _symbol, string memory _uri) ERC721(_name, _symbol) {
+    uri = _uri;
+  }
+
+  struct GemData {
+    uint gemRank;
+    uint gemType;
+  }
+
+  mapping (uint => GemData) public gemData; 
+
+  function tokenURI(uint _tokenId) override public view returns (string memory){
+    string memory gemRank = Strings.toString(gemData[_tokenId].gemRank);
+    string memory gemType = Strings.toString(gemData[_tokenId].gemType);
+
+    return string (
+      abi.encodePacked(uri, '/', gemRank,'/', gemType,'.json')
+    );
+  }
 
   function mintGemToken() public {
-    uint tokenId = totalSupply() + 1;  // 발행된 NFT 총량
+    uint tokenId = totalSupply() + 1;  
+
+    gemData[tokenId] = GemData(4, 4); // gemRank, gemType 
     _mint(msg.sender,tokenId);
   }
 }
