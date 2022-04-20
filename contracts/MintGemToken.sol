3a9bbe9c1ac05cd258c0cd4b363f01a2cb411701
hyeri <rhkk7157@gmail.com>
2022-04-20 10:23:39 +0900

    opensea check & payable

--- contracts/MintGemToken.sol
+++ contracts/MintGemToken.sol
@@ -3,9 +3,10 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract MintGemToken is ERC721Enumerable {
+contract MintGemToken is ERC721Enumerable, Ownable {
   string uri;
   constructor(string memory _name, string memory _symbol, string memory _uri) ERC721(_name, _symbol) {
     uri = _uri;
@@ -18,6 +19,9 @@ contract MintGemToken is ERC721Enumerable {
 
   mapping (uint => GemData) public gemData; 
 
+  // 1 klay
+  uint gemPrice = 1000000000000000000; // PEB
+
   function tokenURI(uint _tokenId) override public view returns (string memory){
     string memory gemRank = Strings.toString(gemData[_tokenId].gemRank);
     string memory gemType = Strings.toString(gemData[_tokenId].gemType);
@@ -27,10 +31,13 @@ contract MintGemToken is ERC721Enumerable {
     );
   }
 
-  function mintGemToken() public {
+  function mintGemToken() public payable {
+    require(gemPrice <= msg.value, "Not enough Klay.");
     uint tokenId = totalSupply() + 1;  
 
-    gemData[tokenId] = GemData(4, 4); // gemRank, gemType 
+    payable(owner()).transfer(msg.value);
+
+    gemData[tokenId] = GemData(3, 3); // gemRank, gemType 
     _mint(msg.sender,tokenId);
   }
 }
